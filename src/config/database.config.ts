6ace75e46c23a0ef70ed86421be29306019b7945
lyhduc <lyhongduc.34@gmail.com>
2024-12-08 20:44:53 +0700

    Config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,8 +5,7 @@ import { config } from 'dotenv';
 config();
 
 export default registerAs('database', (): TypeOrmModuleOptions => ({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
+    host: process.env.MYSQL_SERVICE_HOST || process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
